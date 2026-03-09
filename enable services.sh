3d1c4c40de66d0f241eee2712de5bf58dd4e3c7d
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-09 16:11:20 +0700

    Update enable services.sh

--- enable services.sh
+++ enable services.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 exec sudo systemctl --user enable hyprpolkitagent.service
 exec sudo systemctl enable sddm.service
-exec systemctl --user enable xdg-desktop-portal-gtk.service
-exec systemctl --user enable swww.service
+exec sudo systemctl --user enable xdg-desktop-portal-gtk.service
+exec sudo systemctl --user enable swww.service
+exec sudo systemctl enable bluetooth
  
