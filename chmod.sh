231696aef62621059cea3c28f4bac98ffbb3f255
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-09 18:28:39 +0700

    Update chmod.sh

--- chmod.sh
+++ chmod.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec chmod +x ~/dot-files-nils/'enable services.sh' & exec chmod +x ~/dot-files-nils/copy.sh & exec chmod +x ~/dot-files-nils/sddm.sh & exec chmod + x ~/dot-files-nils/install.sh
+exec chmod +x ~/dot-files-nils/'enable services.sh' & exec chmod +x ~/dot-files-nils/copy.sh & exec chmod +x ~/dot-files-nils/sddm.sh & exec chmod +x ~/dot-files-nils/install.sh
