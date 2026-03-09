e4f438fd167ad71dc3f247a626998cf6c419269b
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-09 16:13:15 +0700

    Update enable services.sh

--- enable services.sh
+++ enable services.sh
@@ -4,4 +4,5 @@ exec sudo systemctl enable sddm.service
 exec sudo systemctl --user enable xdg-desktop-portal-gtk.service
 exec sudo systemctl --user enable swww.service
 exec sudo systemctl enable bluetooth
+exec sudo systemctl --user enable pipewire pipewire-pulse wireplumber
  
