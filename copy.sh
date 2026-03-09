f040c58a0afa78c612ab14bc8a2f026a397bfb15
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-09 16:55:09 +0700

    Create copy.sh

---
+++ copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+exec cp -r -v ~/dot-files-nils/hypr ~/.config
+exec cp -r -v ~/dot-files-nils/fish ~/.config
+exec cp -r -v ~/dot-files-nils/kitty ~/.config
+exec cp -r -v ~/dot-files-nils/mpd ~/.config
+exec cp -r -v ~/dot-files-nils/rofi ~/.config
+exec cp -r -v ~/dot-files-nils/systemd ~/.config
+exec cp -r -v ~/dot-files-nils/waybar ~/.config
