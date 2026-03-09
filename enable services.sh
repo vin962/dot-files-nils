baf56718b0261acf5ee739aaacfb51f435892555
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-09 16:01:53 +0700

    Update enable services.sh

--- enable services.sh
+++ enable services.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-exec systemctl enable hyprpolkitagent.service
+exec sudo systemctl enable hyprpolkitagent.service
 
-exec sudo systemctl enable sddm.service   
+exec sudo systemctl enable sddm.service
+
+exec systemctl enable xdg-desktop-portal-gtk.service   
