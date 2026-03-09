e94448d3c702996cc74bdc448f97840005c4de1b
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-09 17:59:43 +0700

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-exec cp -r -v ~/dot-files-nils/hypr ~/.config
-exec cp -r -v ~/dot-files-nils/fish ~/.config
-exec cp -r -v ~/dot-files-nils/kitty ~/.config
-exec cp -r -v ~/dot-files-nils/mpd ~/.config
-exec cp -r -v ~/dot-files-nils/rofi ~/.config
-exec cp -r -v ~/dot-files-nils/systemd ~/.config
-exec cp -r -v ~/dot-files-nils/waybar ~/.config
+exec cp -r ~/dot-files-nils/hypr ~/.config & exec cp -r ~/dot-files-nils/fish ~/.config & exec cp -r ~/dot-files-nils/kitty ~/.config & exec cp -r ~/dot-files-nils/mpd ~/.config & exec cp -r ~/dot-files-nils/rofi ~/.config & exec cp -r ~/dot-files-nils/systemd ~/.config & exec cp -r ~/dot-files-nils/waybar ~/.config &
