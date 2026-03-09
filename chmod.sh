ada918cce6466177d528252e58457bc43fb194a5
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-09 18:26:32 +0700

    Create chmod.sh

---
+++ chmod.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec chmod +x ~/dot-files-nils/enable services.sh & exec chmod +x ~/dot-files-nils/copy.sh & exec chmod +x ~/dot-files-nils/sddm.sh & exec chmod + x ~/dot-files-nils/install.sh
