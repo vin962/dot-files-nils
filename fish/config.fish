ef3a9764f48e6131318f7930675eed3778bf3a77
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-09 15:16:18 +0700

    Add files via upload
    Signed-off-by: Blikizz <61079205+vin962@users.noreply.github.com>

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,6 @@
 set fish_greeting
 fish_vi_key_bindings
 
-#===============================================#
-#           aliases and functions
-#===============================================#
-source ~/.config/fish/conf.d/aliases.fish
-source ~/.config/fish/functions/functions.fish
-source ~/.config/fish/conf.d/personal.fish
-
 if command -v fastfetch > /dev/null
     # Only run fastfetch if we're in an interactive shell
     if status --is-interactive
