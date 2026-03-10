de8b1c71db2286750a364002c07d9c5cbe510afa
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-10 14:44:44 +0700

    Add config directory copy to copy.sh
    Signed-off-by: Blikizz <61079205+vin962@users.noreply.github.com>

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec cp -r ~/dot-files-nils/hypr ~/.config & cp -r ~/dot-files-nils/fish ~/.config & cp -r ~/dot-files-nils/kitty ~/.config & cp -r ~/dot-files-nils/mpd ~/.config & cp -r ~/dot-files-nils/rofi ~/.config & cp -r ~/dot-files-nils/systemd ~/.config & cp -r ~/dot-files-nils/waybar ~/.config & cp -r ~/dot-files-nils/fastfetch ~/.config
+exec cp -r ~/dot-files/config ~/.config & cp -r ~/dot-files-nils/hypr ~/.config & cp -r ~/dot-files-nils/fish ~/.config & cp -r ~/dot-files-nils/kitty ~/.config & cp -r ~/dot-files-nils/mpd ~/.config & cp -r ~/dot-files-nils/rofi ~/.config & cp -r ~/dot-files-nils/systemd ~/.config & cp -r ~/dot-files-nils/waybar ~/.config & cp -r ~/dot-files-nils/fastfetch ~/.config
