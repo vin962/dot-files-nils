dda8ad55a0b10f48c9c735a38506ca339282102c
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-10 15:52:36 +0700

    Create theme.sh
    Signed-off-by: Blikizz <61079205+vin962@users.noreply.github.com>

---
+++ theme.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec sudo tar -xvf ~/theme/candy-icons.tar.xz -C /usr/share/icons/ & tar -xvf ~/theme/Sweet-Ambar-Blue-Dark-v40.tar.xz -C /usr/share/themes/ & tar -xvf ~/theme/Sweet-Ambar-Blue-Plasma-6.tar.xz -C /usr/share/sddm/themes
