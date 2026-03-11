a02b5aa2c8256e0f31d7b7400958d82d7017be9a
Blikizz <61079205+vin962@users.noreply.github.com>
2026-03-11 13:17:44 +0700

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec sudo tar -xvf ~/dot-files-nils/theme/candy-icons.tar.xz -C /usr/share/icons/ & exec sudo tar -xvf ~/dot-files-nils/theme/Sweet-Ambar-Blue-Dark-v40.tar.xz -C /usr/share/themes/ & exec sudo tar -xvf ~/dot-files-nils/theme/SilentSDDM-1.4.2.tar.gz -C /usr/share/sddm/themes & exec cp ~/dot-files-nils/'dolphin and kate conf files' /etc 
+exec sudo tar -xvf ~/dot-files-nils/theme/candy-icons.tar.xz -C /usr/share/icons/ & exec sudo tar -xvf ~/dot-files-nils/theme/Sweet-Ambar-Blue-Dark-v40.tar.xz -C /usr/share/themes/ & exec sudo tar -xvf ~/dot-files-nils/theme/SilentSDDM-1.4.2.tar.gz -C /usr/share/sddm/themes & exec cp ~/dot-files-nils/'dolphin and kate conf files'/sddm.conf /etc 
